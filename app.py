fd0481eb8b74c8a14c36552b5731c5585cb436df
Dwafsigil <jtu02@jaguar.tamu.edu>
2025-11-17 17:30:18 -0600

    Change naming convention to match folder file properly

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 from pickle import load
 import os
 
-app = Flask(__name__, template_folder='../templates')
+app = Flask(__name__, template_folder='./template')
 
 model_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "./models/decision_tree_classifier_default_42.sav")
 model = load(open(model_path, "rb"))
